a3550177330c91641daade8fac243d0c42259fc1
jnaadjie <james.naadjie@gmail.com>
2015-12-04 16:35:47 -0500

    Test list immutability

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,6 +1,20 @@
-import {expect} from 'chai'
+import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('immutability', () => {
+  describe('A List', () => {
+    function addMovie(currentState,movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of('Trainspotting', '28 days Later', 'Sunshine'));
+      expect(state).to.equal(List.of('Trainspotting', '28 days Later'));
+    });
+  });
   describe('a number', () => {
     function increment(currentState) {
       return currentState + 1;
